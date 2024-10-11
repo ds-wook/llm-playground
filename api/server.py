33e417752b4155e872ec87672341c234eae97d62
ds-wook <leewook94@gmail.com>
2024-10-11 16:51:32 +0900

    feat: remove Name class

--- api/server.py
+++ api/server.py
@@ -1,16 +1,11 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
 from api.gemini_api import predict_response
 
 app = FastAPI()
 templates = Jinja2Templates(directory="api/templates")
-
-
-class NameValues(BaseModel):
-    Check: str = None
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -19,7 +14,7 @@ def home(request: Request):
 
 
 @app.post("/gemini", response_class=HTMLResponse)
-async def handle_form(request: Request, Check: str = Form(...)):
+async def post_gemini(request: Request, Check: str = Form(...)):
     # POST로 받은 데이터 처리
     prediction = predict_response(Check)
     return templates.TemplateResponse("llm.html", {"request": request, "prediction": prediction})
